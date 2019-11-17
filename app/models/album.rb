cb76052dbfce7752b0eec32cb196bed43dcdbea2
Thiago Lopes <thiagodaniellopes99@gmail.com>
2019-11-17 11:41:32 -0300

    Adiciona filtro pra artist_id

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,7 @@ class Album < ApplicationRecord
   belongs_to :artist
   has_many :tracks
 
-    scope :filter, ->(title) { where("title LIKE ?", "%#{title}%") if title.present? }
-    scope :artist, ->(name) { where("name LIKE ?", "%#{name}%") if name.present? }
+    scope :filter_title, ->(title) { where("title LIKE ?", "%#{title}%") if title.present? }
+    scope :filter_artist_id, ->(artist_id) { where("artist_id LIKE ?", "%#{artist_id}%") if artist_id.present? }
     validates_presence_of :title, :artist
 end
