162a65fd8e8349f81ea79439e9f1f6e123090e0e
Thiago Lopes <thiagodaniellopes99@gmail.com>
2019-11-18 18:10:07 -0300

    Adiciona validação de bytes

--- app/models/track.rb
+++ app/models/track.rb
@@ -7,6 +7,6 @@ class Track < ApplicationRecord
   validates_presence_of :name, :album, :media_type, :genre,
                         :milliseconds, :bytes, :unit_price
   validates :milliseconds, :unit_price, :numericality => { :greater_than_or_equal_to => 0}
-  validates :bytes, :numericality =>{ :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2147483647 }
+  validates :bytes, :numericality =>{ :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2147483647}
 
 end
